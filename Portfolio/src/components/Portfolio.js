207aec5bae16e78d6df3c6a196f88ec13b7ff1f0
Ronald chen <rc9525@gmail.com>
2022-04-29 17:44:46 -0400

    js

---
+++ Portfolio/src/components/Portfolio.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Portfolio(props) {
+    return (
+        <div>
+            <a href="http://www.google.com"><img src="https://via.placeholder.com/150" /></a>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate
+          quam aliquid blanditiis aperiam exercitationem doloremque voluptatum
+          qui expedita quidem numquam, laboriosam similique excepturi beatae
+          tempora dolorum repudiandae vel voluptas esse.
+        </p>
+
+        <a href="http://www.google.com"><img src="https://via.placeholder.com/150" /></a>
+
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptate
+          quam aliquid blanditiis aperiam exercitationem doloremque voluptatum
+          qui expedita quidem numquam, laboriosam similique excepturi beatae
+          tempora dolorum repudiandae vel voluptas esse.
+        </p>
+        </div>
+    );
+}
+
+export default Portfolio;
